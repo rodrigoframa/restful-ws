498d46a0356ad096e55310cd0a268ba1e5d8fa21
Rodrigo Frazão Maia <rodrigo.frazao@armateus.com.br>
2018-04-19 14:22:24 -0300

    Primeiro commit

---
+++ src/http/routes.js
@@ -0,0 +1,58 @@
+
+const db = require('../services/mysql')
+
+const routes = (server) => {
+  server.get('categoria', async (req, res, next) => {
+    try {
+      res.send(await db.categories().all())
+      next()
+    } catch (error) {
+      res.send(error)
+      next()
+    }
+    // db.categories().all().then(categories => {
+    //   res.send(categories)
+    //   next()
+    // }).catch(error => {
+    //   res.send(error)
+    //   next()
+    // })
+  })
+
+  server.post('categoria', async (req, res, next) => {
+    const { name } = req.params
+    try {
+      res.send(await db.categories().save(name))
+      next()
+    } catch (error) {
+      res.send(error)
+      next()
+    }
+  })
+  server.put('categoria', async (req, res, next) => {
+    const { id, name } = req.params
+    try {
+      res.send(await db.categories().update(id, name))
+      next()
+    } catch (error) {
+      res.send(error)
+      next()
+    }
+  })
+  server.del('categoria', async (req, res, next) => {
+    const { id } = req.params
+    try {
+      res.send(await db.categories().delete(id))
+      next()
+    } catch (error) {
+      res.send(error)
+      next()
+    }
+  })
+  server.get('/', (req, res, next) => {
+    res.send('Enjoy the silence!')
+    next()
+  })
+}
+
+module.exports = routes
